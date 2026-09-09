c826e748d8ac4c8fa05ae953d7a62e52acb4bbfa
Muhammad Arsalan <arsalan.muhammad01.12@gmail.com>
2026-09-09 11:47:05 +0500

    api_key added to the streamlit secrets

--- chain.py
+++ chain.py
@@ -1,13 +1,17 @@
+import streamlit as st
 from prompts import get_prompt_template, CarDealEvaluation
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+
+groq_api_key = os.getenv("GROQ_API_KEY") or st.secrets.get("GROQ_API_KEY")
 
 def evaluate_car(make, model, year, mileage, asking_price, city, car_deal_description):
     prompt = get_prompt_template()
     
-    llm = ChatGroq(model="openai/gpt-oss-120b", temperature=0.3)
+    llm = ChatGroq(model="openai/gpt-oss-120b", temperature=0.3, groq_api_key=groq_api_key)
     structured_llm = llm.with_structured_output(CarDealEvaluation)
 
     chain = prompt | structured_llm
